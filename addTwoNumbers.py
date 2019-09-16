dcf2562fba4ce1400bae47dd67704d78bf126716
Piu Mallick <piu.mallick@outlook.com>
2019-09-16 13:58:44 -0400

    Add files via upload

---
+++ addTwoNumbers.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Python program to add two  numbers
+
+# Input numbers
+num1 = input("Provide the first number: ")
+num2 = input("Provide the second number: ")
+
+# Adding two numbers
+sum = float(num1) + float(num2)
+
+# Printing the sum
+print("\nThe sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+
+"""
+Output:
+    
+Provide the first number: 2.4
+
+Provide the second number: 5.7
+
+The sum of 2.4 and 5.7 is 8.1
+
+"""
