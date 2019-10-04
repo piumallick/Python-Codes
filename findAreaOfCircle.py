35a8783273bf95cac3b0e4c5a9c37730857500fe
Piu Mallick <piu.mallick@outlook.com>
2019-10-04 14:22:16 -0400

    Finding Area of Circle

---
+++ findAreaOfCircle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 14:17:05 2019
+
+@author: piumallick
+"""
+
+# Python Program for Program to find area of a circle
+
+def findAreaOfCircle(radius):
+    pi = 3.14
+    area = pi * (radius * radius)
+    return area;
+
+# Driver code
+print('Area of the circle is', findAreaOfCircle(6));
+
+# Output:
+# Area of the circle is 113.04
+    
+    
